e866523b6aa92dd052ec2f76c424448c914919ff
Brittney Contreras <51245855+bcontreras4@users.noreply.github.com>
2023-03-14 12:52:48 -0600

    Update docker_wrapper.py
    Ignoring scenarios in the binned seed runs

--- scripts/docker_wrapper.py
+++ scripts/docker_wrapper.py
@@ -63,59 +63,95 @@ def wrapper_run(args: dict):
         'outdir': outdir,
         'pickle_matrices': False
     }
+    
+        # MODEL FITTING
+    # This code is mostly just copied from the Jupyter notebooks we use, but in the future we can make this
+    # a more general wrapper for doing model fitting and generating plots.
+    base_model = do_single_fit(**base_model_args)
+    #base_model = RMWCovidModel(base_spec_id=4887)
+    #base_model.prep()
+    base_model.solve_seir()
+    with open(get_filepath_prefix(outdir, tags=base_model.tags) + f"model_solutionydf.pkl", "wb") as f:
+        pickle.dump(base_model.solution_ydf, f)
+    #base_model.solve_seir()
 
-    scenario_fit_args = {
-        'outdir': outdir,
-        'start_date': args["start_date"],
-        'end_date': args["end_date"],
-        'fit_start_date': scenario_fit_start_date,
-        'fit_end_date': args["scenarios_fit_end_date"],
-        'model_class': RMWCovidModel,
-        'write_results': False,
-        'pickle_matrices': False,
-        'pre_solve_model': True
-    }
+    # MODEL OUTPUTS
+    logging.info('Projecting')
+    do_create_report(base_model, outdir=outdir, prep_model=False, solve_model=False,
+                     immun_variants=args["report_variants"],
+                     from_date=args["report_start_date"])
+    base_model.solution_sum_df(['seir', 'variant', 'immun']).unstack().to_csv(
+        get_filepath_prefix(outdir, tags=base_model.tags) + 'seir_variant_immun_total_all_at_once_projection.csv')
+    base_model.solution_sum_df().unstack().to_csv(
+        get_filepath_prefix(outdir, tags=base_model.tags) + 'full_projection.csv')
+    xmin = datetime.datetime.strptime(args["start_date"], "%Y-%m-%d").date()
+    xmax = datetime.datetime.strptime(args["end_date"], "%Y-%m-%d").date()
+    fig = plt.figure(figsize=(10, 10), dpi=300)
+    ax = fig.add_subplot(211)
+    hosps_df = base_model.modeled_vs_observed_hosps().reset_index('region').drop(columns='region')
+    hosps_df.plot(ax=ax)
+    ax.set_xlim(xmin, xmax)
+    ax = fig.add_subplot(212)
+    plot_transmission_control(base_model, ax=ax)
+    ax.set_xlim(xmin, xmax)
+    plt.savefig(get_filepath_prefix(outdir, tags=base_model.tags) + '_model_forecast.png')
+    plt.close()
+    hosps_df.to_csv(get_filepath_prefix(outdir, tags=base_model.tags) + '_model_forecast.csv')
+    json.dump(base_model.tc, open(get_filepath_prefix(outdir, tags=base_model.tags) + 'model_forecast_tc.json', 'w'))
+    
+    exit(0)
+    #scenario_fit_args = {
+        #'outdir': outdir,
+        #'start_date': args["start_date"],
+        #'end_date': args["end_date"],
+        #'fit_start_date': scenario_fit_start_date,
+        #'fit_end_date': args["scenarios_fit_end_date"],
+        #'model_class': RMWCovidModel,
+        #'write_results': False,
+        #'pickle_matrices': False,
+        #'pre_solve_model': True
+    #}
     # Set up the arguments for the scenario fits.
-    scenario_model_args = []
-    scenario_params = json.load(open(base_model_args["params_defs"]))
+    #scenario_model_args = []
+    #scenario_params = json.load(open(base_model_args["params_defs"]))
     # ,  (0.8, 0.2, 0.04, 0.02), (0.95, 0.5, 0.1, 0.08),(0.9, 0.3, 0.05, 0.03)
     # +/- 20% for all parameters
-    orig_params = np.array([0.12, 0.42, 0.24, 0.86])
-    plus_20 = orig_params*1.2
-    minus_20 = orig_params*0.8
-    for (strong_omc_escape, weak_omc_escape, strong_escape, weak_escape) in [orig_params,plus_20,minus_20]:
-        weak_omicron_param = [{"param": "immune_escape",
-                       "from_attrs": {"immun": "weak",
-                                      "variant": ["omicron", "ba2", "ba2121", "ba45","bq"]},
-                       "to_attrs": {"variant": ["xbb"]},
-                       "vals": {"2020-01-01": weak_omc_escape},
-                       "desc": "emerging variants immune escape value, weak immunity"}]
-        strong_omicron_param = [{"param": "immune_escape",
-                         "from_attrs": {"immun": "strong",
-                                        "variant": ["omicron", "ba2", "ba2121", "ba45", "bq"]},
-                         "to_attrs": {"variant": ["xbb"]},
-                         "vals": {"2020-01-01": strong_omc_escape},
-                         "desc": "emerging variants immune escape value, strong immunity"}]
-        weak_param = [{"param": "immune_escape",
-                       "from_attrs": {"immun": "weak",
-                                      "variant": ["none", "wildtype", "alpha", "delta"]},
-                       "to_attrs": {"variant": ["xbb"]},
-                       "vals": {"2020-01-01": weak_escape},
-                       "desc": "emerging variants immune escape value, weak immunity"}]
-        strong_param = [{"param": "immune_escape",
-                         "from_attrs": {"immun": "strong",
-                                        "variant": ["none", "wildtype", "alpha", "delta"]},
-                         "to_attrs": {"variant": ["xbb"]},
-                         "vals": {"2020-01-01": strong_escape},
-                         "desc": "emerging variants immune escape value, strong immunity"}]
-        scenario_model_args.append({'params_defs': scenario_params + weak_param + strong_param + weak_omicron_param + strong_omicron_param,
-                                    'tags': {'xbb_esc_omc_strong': strong_omc_escape,
-                                             'xbb_esc_omc_weak': weak_omc_escape,
-                                             'xbb_esc_strong': strong_escape,
-                                             'xbb_esc_weak': weak_escape}})
+    #orig_params = np.array([0.12, 0.42, 0.24, 0.86])
+    #plus_20 = orig_params*1.2
+    #minus_20 = orig_params*0.8
+    #for (strong_omc_escape, weak_omc_escape, strong_escape, weak_escape) in [orig_params,plus_20,minus_20]:
+        #weak_omicron_param = [{"param": "immune_escape",
+                       #"from_attrs": {"immun": "weak",
+                                      #"variant": ["omicron", "ba2", "ba2121", "ba45","bq"]},
+                       #"to_attrs": {"variant": ["xbb"]},
+                       #"vals": {"2020-01-01": weak_omc_escape},
+                       #"desc": "emerging variants immune escape value, weak immunity"}]
+        #strong_omicron_param = [{"param": "immune_escape",
+                         #"from_attrs": {"immun": "strong",
+                                        #"variant": ["omicron", "ba2", "ba2121", "ba45", "bq"]},
+                         #"to_attrs": {"variant": ["xbb"]},
+                         #"vals": {"2020-01-01": strong_omc_escape},
+                         #"desc": "emerging variants immune escape value, strong immunity"}]
+        #weak_param = [{"param": "immune_escape",
+                       #"from_attrs": {"immun": "weak",
+                                      #"variant": ["none", "wildtype", "alpha", "delta"]},
+                       #"to_attrs": {"variant": ["xbb"]},
+                       #"vals": {"2020-01-01": weak_escape},
+                       #"desc": "emerging variants immune escape value, weak immunity"}]
+        #strong_param = [{"param": "immune_escape",
+                         #"from_attrs": {"immun": "strong",
+                                        #"variant": ["none", "wildtype", "alpha", "delta"]},
+                         #"to_attrs": {"variant": ["xbb"]},
+                         #"vals": {"2020-01-01": strong_escape},
+                         #"desc": "emerging variants immune escape value, strong immunity"}]
+        #scenario_model_args.append({'params_defs': scenario_params + weak_param + strong_param + weak_omicron_param + strong_omicron_param,
+                                    #'tags': {'xbb_esc_omc_strong': strong_omc_escape,
+                                             #'xbb_esc_omc_weak': weak_omc_escape,
+                                             #'xbb_esc_strong': strong_escape,
+                                             #'xbb_esc_weak': weak_escape}})
     # SET SPEC IDs
     # Number of fits is the number of scenarios plus the base model fit
-    n_fits = len(scenario_model_args) + 1
+    #n_fits = len(scenario_model_args) + 1
     spec_ids = [START_SPEC_ID + (BATCH_TASK_INDEX * n_fits) + i for i in range(n_fits)]
     base_model_args["spec_id"] = spec_ids[0]
     for scen, spec_id in zip(scenario_model_args, spec_ids[1:]):
